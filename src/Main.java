723f49a5f2d2e770d88403535a329e3559c093cc
samuellgs.dev <samuellds.dev@gmail.com>
2023-09-11 20:14:58 -0300

    Fix bug

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.util.
 import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main (String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o titulo do livro: ");
+        String titulo = scanner.next();
+
+        System.out.println("Digite o autor: ");
+        String autor = scanner.next();
+
+        System.out.println("Digite a editora: ");
+        String editora = scanner.next();
+
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.adicionarLivro(new Livro(titulo, autor, editora));
+
+        biblioteca.listarLivros();
     }
+
 }
