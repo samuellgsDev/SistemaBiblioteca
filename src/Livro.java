68d18dd8d0325a980ef8978088d2a483395102a0
samuellgs.dev <samuellds.dev@gmail.com>
2023-09-13 20:46:51 -0300

    fix - add new method

--- src/Livro.java
+++ src/Livro.java
@@ -1,9 +1,11 @@
 public class Livro {
+    //private int id;
     private String titulo;
     private String autor;
     private String editora;
 
     public Livro(String titulo, String autor, String editora) {
+        //this.id = id;
         this.titulo = titulo; //this é usado para acessar o atributo da instância atual
         this.autor = autor;
         this.editora = editora;
@@ -12,7 +14,7 @@ public class Livro {
     public String getAutor() {return autor;}
     public String getEditora() {return editora;}
 
-    public String toString() { //toString é usado para representar o livro em formatoi de string
+    public String toString() { //toString é usado para representar o livro em formato de string
     return  "Livro{" +
             "titulo='" + titulo + '\'' +
             ", autor='" + autor + '\'' +
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,33 @@ public class Main {
     public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a opção que você deseja:\n 1: Adicionar Livro \n 2: Ler Livro \n 3: Alterar livro \n 4: Remover Livro \n 5: Sair.");
 
-        System.out.println("Digite o titulo do livro: ");
-        String titulo = scanner.next();
+        while (true) {
+            System.out.println("\nDigite uma opção de 1 a 5: ");
+            int opcao = scanner.nextInt();
 
-        System.out.println("Digite o autor: ");
-        String autor = scanner.next();
+            Biblioteca biblioteca = new Biblioteca();
 
-        System.out.println("Digite a editora: ");
-        String editora = scanner.next();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o titulo do livro: ");
+                    String titulo = scanner.nextLine();
 
-        Biblioteca biblioteca = new Biblioteca();
-        biblioteca.adicionarLivro(new Livro(titulo, autor, editora));
-        biblioteca.listarLivros();
+                    System.out.println("Digite o autor: ");
+                    String autor = scanner.nextLine();
+
+                    System.out.println("Digite a editora: ");
+                    String editora = scanner.nextLine();
+
+                    Livro livro = new Livro(titulo, autor, editora);
+                    biblioteca.adicionarLivro(new Livro(titulo, autor, editora));
+                    break;
+                case 2:
+                    biblioteca.listarLivros();
+                    break;
+
+            }
+        }
     }
-
 }
